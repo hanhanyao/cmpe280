bdaea3c85fd60fe7eb7757320a5067a41508de83
aaronlam88 <aaron.y.lam@sjsu.edu>
2018-09-06 12:02:46 -0700

    save progress

--- javascript/webapp/index.js
+++ javascript/webapp/index.js
@@ -1,12 +1,14 @@
-// import require modules 
-import React from 'react';
-import { render } from 'react-dom';
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
+import Home from './components/home/Home.jsx'
+import About from './components/about/About.jsx'
+import Contact from './components/contact/Contact.jsx'
 
-// import components
-import HelloWorld from'./components/helloWorld/HelloWorld.jsx';
-
-render(
-   <HelloWorld
-        name = 'WebKeepers'
-   />,
-    document.getElementById('mainWrapper'));
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Home}/>
+    <Route path="/repos" component={Contact}/>
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('app'))
